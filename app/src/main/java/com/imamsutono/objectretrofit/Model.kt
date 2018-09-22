5cdde49970ba26308ca3cb577180379aa0c3f932
imamsutono <imamssutono@gmail.com>
2018-09-22 15:15:20 +0700

    Add model

--- app/src/main/java/com/imamsutono/objectretrofit/Model.kt
+++ app/src/main/java/com/imamsutono/objectretrofit/Model.kt
@@ -1,2 +1,23 @@
 package com.imamsutono.objectretrofit
 
+import com.google.gson.annotations.SerializedName
+
+data class Model(
+        @SerializedName("status") // harus sama dengan key pada json
+        val status: Boolean?,
+        @SerializedName("message")
+        val message: String?,
+        @SerializedName("arrayInt")
+        val arrayInt: MutableList<Int>?,
+        @SerializedName("arrayString")
+        val arrayString: MutableList<String>?,
+        @SerializedName("data")
+        val data: MutableList<Data>?
+)
+
+data class Data(
+        @SerializedName("id")
+        val id: String?,
+        @SerializedName("nama")
+        val nama: String?
+)
