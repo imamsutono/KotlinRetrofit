8da3a5ddcdd1c1800d1ccb1c73ee59d5ab0e609b
imamsutono <imamssutono@gmail.com>
2018-09-22 15:37:10 +0700

    Add service interface

--- app/src/main/java/com/imamsutono/objectretrofit/ServiceInterface.kt
+++ app/src/main/java/com/imamsutono/objectretrofit/ServiceInterface.kt
@@ -1,2 +1,10 @@
 package com.imamsutono.objectretrofit
 
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ServiceInterface {
+
+    @GET("tesapi/index.php/MainRoutes")
+    fun getData(): Call<Model>
+}
